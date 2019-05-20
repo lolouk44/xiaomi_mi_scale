83dd5a886c29fc0e82c1a1301322978e04616e0a
lolouk44 <laurent.mahe@gmail.com>
2019-05-20 12:08:22 +0100

    update to include body measurements

--- Xiaomi_Scale.py
+++ Xiaomi_Scale.py
@@ -1,3 +1,19 @@
+#############################################################################################
+# Code to read weight measurements fom Xiaomi Scale V2
+# (Framework is prsent to also read from Xiaomi Scale V1, though I do not own one to test so code has not been maintained)
+# Must be executed with Python 3 else body measurements are incorrect.
+# Must be executed as root, therefore best to schedule via crontab every 5 min (so as not to drain the battery):
+# */5 * * * * python3 /path-to-script/Xiaomi_Scale.py
+# Multi user possible as long as weitghs do not overlap, see lines 117-131
+#
+# Thanks to @syssi (https://gist.github.com/syssi/4108a54877406dc231d95514e538bde9) and @prototux (https://github.com/wiecosystem/Bluetooth) for their initial code
+#
+# Make sure you edit MQTT credentials below and user logic/data on lines 117-131
+#
+#############################################################################################
+
+
+
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from __future__ import print_function
@@ -10,6 +26,8 @@ from bluepy import btle
 import paho.mqtt.client as mqtt
 from datetime import datetime
 
+import Xiaomi_Scale_Body_Metrics
+
 MISCALE_MAC = 'REDACTED'
 MQTT_USERNAME = 'REDACTED'
 MQTT_PASSWORD = 'REDACTED'
@@ -17,24 +35,13 @@ MQTT_HOST = 'REDACTED'
 MQTT_PORT = 1883
 MQTT_TIMEOUT = 60
 
-if os.getenv('C', '1') == '0':
-	ANSI_RED = ''
-	ANSI_GREEN = ''
-	ANSI_YELLOW = ''
-	ANSI_CYAN = ''
-	ANSI_WHITE = ''
-	ANSI_OFF = ''
-else:
-	ANSI_CSI = "\033["
-	ANSI_RED = ANSI_CSI + '31m'
-	ANSI_GREEN = ANSI_CSI + '32m'
-	ANSI_YELLOW = ANSI_CSI + '33m'
-	ANSI_CYAN = ANSI_CSI + '36m'
-	ANSI_WHITE = ANSI_CSI + '37m'
-	ANSI_OFF = ANSI_CSI + '0m'
-
 
 class ScanProcessor():
+
+	def GetAge(self, d1):
+		d1 = datetime.strptime(d1, "%Y-%m-%d")
+		d2 = datetime.strptime(datetime.today().strftime('%Y-%m-%d'),'%Y-%m-%d')
+		return abs((d2 - d1).days)/365
 
 	def __init__(self):
 		self.mqtt_client = None
@@ -109,30 +116,47 @@ class ScanProcessor():
 			raise Exception('not connected to MQTT server')
 		if int(weight) > 72:
 			user="lolo"
+			height=175
+			age=self.GetAge("1900-01-01")
+			sex="male"
 		elif int(weight) < 50:
 			user="kiaan"
+			height=103
+			age=self.GetAge("1900-01-01")
+			sex="male"
 		else:
 			user = "div"
-		prefix = '{}/{}'.format(user+'/weight', unit)
-		self.mqtt_client.publish(prefix, weight, qos=1, retain=True)
-		print('\tSent data to topic %s: %s %s' % (prefix, weight, unit))
-		if miimpedance:
-			prefix = '{}/{}'.format(user+'/weight', "Timestamp")
-			self.mqtt_client.publish(prefix, mitdatetime, qos=1, retain=True)
-			if int(miimpedance) < 6000: #scale sends fffd (65533) if impedance could not be measured
-				prefix = '{}/{}'.format(user+'/weight', "Impedance")
-				self.mqtt_client.publish(prefix, miimpedance, qos=1, retain=True)
+			height=170
+			age=self.GetAge("1900-01-01")
+			sex="female"
+		lib = Xiaomi_Scale_Body_Metrics.bodyMetrics(weight, height, age, sex, 0)
+		message = '{'
+		message += '"Weight":"' + "{:.2f}".format(weight) + '"'
+		message += ',"BMI":"' + "{:.2f}".format(lib.getBMI()) + '"'
+		message += ',"Basal Metabolism":"' + "{:.2f}".format(lib.getBMR()) + '"'
+		message += ',"Visceral Fat":"' + "{:.2f}".format(lib.getVisceralFat()) + '"'
 
+		if miimpedance:
+			lib = Xiaomi_Scale_Body_Metrics.bodyMetrics(weight, height, age, sex, int(miimpedance))
+			message += ',"Lean Body Mass":"' + "{:.2f}".format(lib.getLBMCoefficient()) + '"'
+			message += ',"Body Fat":"' + "{:.2f}".format(lib.getFatPercentage()) + '"'
+			message += ',"Water":"' + "{:.2f}".format(lib.getWaterPercentage()) + '"'
+			message += ',"Bone Mass":"' + "{:.2f}".format(lib.getBoneMass()) + '"'
+			message += ',"Muscle Mass":"' + "{:.2f}".format(lib.getMuscleMass()) + '"'
+			message += ',"Protein":"' + "{:.2f}".format(lib.getProteinPercentage()) + '"'
+			self.mqtt_client.publish(user, weight, qos=1, retain=True)
+
+		message += ',"TimeStamp":"' + mitdatetime + '"'
+		message += '}'
+		self.mqtt_client.publish(user+'/weight', message, qos=1, retain=True)
+		print('\tSent data to topic %s: %s' % (user+'/weight', message))
 
 def main():
 
 	# while(True):
 	scanner = btle.Scanner().withDelegate(ScanProcessor())
 
-	# print (ANSI_RED + "Scanning for devices..." + ANSI_OFF)
 	devices = scanner.scan(5)
-		# time.sleep(300)
-	# devices = scanner.scan(5)
 
 if __name__ == "__main__":
 	main()
