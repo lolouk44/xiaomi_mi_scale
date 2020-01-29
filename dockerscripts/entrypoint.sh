ac91733eab48837e2f73e5e4d09828c7095a7d55
lolouk44 <laurent.mahe@gmail.com>
2020-01-29 15:24:09 +0000

    no cron

--- dockerscripts/entrypoint.sh
+++ dockerscripts/entrypoint.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
 set -e
-
-rm -rf /var/spool/cron/crontabs && mkdir -m 0644 -p /var/spool/cron/crontabs
-
-[ "$(ls -A /etc/cron.d)" ] && cp -f /etc/cron.d/* /var/spool/cron/crontabs/ || true
-[ ! -z "$CRON_STRINGS" ] && echo -e "$CRON_STRINGS\n" > /var/spool/cron/crontabs/CRON_STRINGS
-
-chmod -R 0644 /var/spool/cron/crontabs
-
 exec "$@"
