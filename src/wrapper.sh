a5ddd5d334fd1968baabd510ef93d47484d31fbb
lolouk44 <laurent.mahe@gmail.com>
2020-12-10 11:36:55 +0000

    Update wrapper.sh

--- src/wrapper.sh
+++ src/wrapper.sh
@@ -2,7 +2,15 @@
 
 sleep 60                      # Give the system time after a reboot to connect to WiFi before continuing
 export MISCALE_MAC=00:00:00:00:00:00 # Mac address of your scale
-export MQTT_PREFIX=miScale
+export HCI_DEV=hci0                  # Bluetooth hci device to use
+export MQTT_HOST=127.0.0.1           # MQTT Server (defaults to 127.0.0.1)
+export MQTT_PREFIX=miscale           # MQTT Topic Prefix. Defaults to miscale
+export MQTT_USERNAME=                # Username for MQTT server (comment out if not required)
+export MQTT_PASSWORD=                # Password for MQTT (comment out if not required)
+export MQTT_PORT=1883                # Defaults to 1883
+export TIME_INTERVAL=30              # Time in sec between each query to the scale, to allow other applications to use the Bluetooth module. Defaults to 30
+export MQTT_DISCOVERY=true           # Home Assistant Discovery (true/false), defaults to true
+export MQTT_DISCOVERY_PREFIX=        # Home Assistant Discovery Prefix, defaults to homeassistant
 
 export USER1_GT=70            # If the weight is greater than this number, we'll assume that we're weighing User #1
 export USER1_SEX=male
